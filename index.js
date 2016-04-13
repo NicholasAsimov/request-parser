f28ac3ce1cd78385981d22b413dbed21648861ea
Nicholas Asimov <kolyaazimov@gmail.com>
2016-04-14 01:13:03 +0300

    Working implementation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express');
+
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.set('views', './views');
 app.set('view engine', 'pug');
 
@@ -12,18 +14,15 @@ app.get('/', (request, response) => {
 
 app.get('/api/whoami', (request, response) => {
   const ipaddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-  // console.log(ip);
+  const software = request.headers['user-agent'].match(/\((.*?)\)/)[1];
 
   response.end(JSON.stringify({
     ipaddress,
     language: request.headers['accept-language'].split(',')[0],
-    software: request.headers['user-agent'],
+    software,
   }));
-
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  /* eslint-disable no-console */
-  console.log(`Server listening on port ${this.address().port}...`);
-  /* eslint-enable no-console */
+app.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}...`);
 });
